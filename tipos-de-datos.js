8e987693ae314c7deab6841b18cce7ff63cb8451
Gabriela Arredondo <gabriela.arq33@gmail.com>
2025-02-04 18:16:08 -0800

    Tema: tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,25 @@
+/// Este es un comentario en JS
+
+//Console.log es una función que se utiliza para imprimir en consola
+console.log("Hola mundo");
+
+//Declaración de variables
+var nombre = "Gaby"; // String
+var edad = 25; // Number
+var sueldo = 1.234 // Number
+var casada = false; // Boolean
+var vacunas = true; // Boolean
+var hijos = null; // Null
+var dato; // Undefined
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+console.log(10 * "Gabby");
+
+// Si ponemos typeof de null dira que es objeto, pero en realidad es ausencia intencional de valor.
